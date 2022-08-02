926fcbf09998f9fe1c591f445bda4502d60f366f
chss3339 <chss3339@gmail.com>
2022-08-03 04:58:30 +0900

    swagger

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import morgan from 'morgan';
 import cors from 'cors';
 import 'dotenv/config';
 import { apiRouter } from './routes/';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
+import path from 'path';
 
 const app = express();
 
@@ -17,7 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname));
-
+const swaggerYaml = YAML.load(path.join(__dirname, './swagger/swagger.yaml'));
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerYaml));
 app.use(
   morgan(
     ':method :url :status :response-time ms - :res[content-length] :date[web]'
