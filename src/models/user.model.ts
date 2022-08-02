aa7ffae20ad6f70f12b58fbad3d1811b63df5295
chss3339 <chss3339@gmail.com>
2022-08-03 04:00:44 +0900

    ConnectionNotFoundError: Connection default was not found. 못풀음

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -9,26 +9,27 @@ export interface UserDbInfo {
 }
 
 export class UserModel {
-  userRepository = getRepository(User);
-
   async find(): Promise<User[]> {
-    const users = await this.userRepository.find();
+    const userRepository = getRepository(User);
+    const users = await userRepository.find();
     return users;
   }
 
   async findById(id: number): Promise<User | null> {
-    const user = await this.userRepository.findOneBy({ id });
+    const userRepository = getRepository(User);
+    const user = await userRepository.findOneBy({ id });
     return user;
   }
 
   async create(userInfo: UserDbInfo): Promise<User> {
-    const newUser = this.userRepository.create(userInfo);
+    const userRepository = getRepository(User);
+    const newUser = userRepository.create(userInfo);
     const errors = await validate(newUser);
     if (errors.length > 0) {
       console.log('에러', errors);
       throw new Error('모든 정보를 입력해 주세요.');
     } else {
-      const createdUser = await this.userRepository.save(newUser);
+      const createdUser = await userRepository.save(newUser);
       return createdUser;
     }
   }
@@ -37,13 +38,15 @@ export class UserModel {
     id: number,
     updateInfo: Partial<UserDbInfo>
   ): Promise<User | null> {
-    await this.userRepository.update({ id }, updateInfo);
-    const updatedUser = await this.userRepository.findOneBy({ id });
+    const userRepository = getRepository(User);
+    await userRepository.update({ id }, updateInfo);
+    const updatedUser = await userRepository.findOneBy({ id });
     return updatedUser;
   }
 
   async delete(id: number): Promise<void> {
-    const result = await this.userRepository.delete({ id });
+    const userRepository = getRepository(User);
+    const result = await userRepository.delete({ id });
     // delete return 값 -> DeleteResult {raw : [], affected : 1}
     // delete가 성공할 경우 affected의 값이 1로 온다.
     if (result.affected === 0) {
